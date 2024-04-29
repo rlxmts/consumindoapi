e6031d27ff9bc8dbeeaa80d7c4f841022fb88cbc
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-29 14:10:07 -0300

    implementando busca

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ async function buscaApi(){
         const api = await  fetch('http://localhost:3000/videos')
         const apiConvertida = await api.json()
         apiConvertida.forEach(video => {
+
+        if(video.categoria == ""){
+            throw new Error("Video sem categoria!");
+        }
+
           listaDeVideos.innerHTML += `
           <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -27,9 +32,34 @@ async function buscaApi(){
             </div>
           </li>`  
         })
-    }catch{
-        listaDeVideos.innerHTML = `<p>Parece que tivemos um problema</p>`
+    }catch(error){
+        listaDeVideos.innerHTML = `<p>Parece que tivemos um problema: ${error}</p>`
     }
 }
 
 buscaApi()
+
+const campoDeBusca = document.querySelector('.pesquisar__input');
+
+campoDeBusca.addEventListener('input', filtrarVideos);
+
+function filtrarVideos(){
+    
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach( video => {
+        
+        let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        let valorPesquisa = campoDeBusca.value.toLowerCase();
+
+        if(valorPesquisa != ""){            
+            if(!titulo.includes(valorPesquisa)){
+                video.style.display = 'none';
+            }else{
+                video.style.display = 'block';
+            }
+        }else{
+            video.style.display = 'block';
+        }
+    })
+
+}
